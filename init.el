16036cc0073c125a2af5b36103745eddcd7474cd
GONNAUD Charles <charles.gonnaud@enedis.fr>
2023-09-25 14:25:12 +0200

    Simplify MELPA section

--- init.el
+++ init.el
@@ -9,25 +9,10 @@
 ;; cf Getting Started https://melpa.org/
 ;; ELPA, the default repository, has much less available
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat "http" "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;;;; use-package : Use package will be used as a package loader in this file
+;;;; use-package : Customize the package macro for this file
 (use-package use-package
   :custom
   (use-package-always-ensure t) ; Download missing packages by default
