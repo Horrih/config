369257c43214d12f5bd484c7a2a5e8152a4cc9d9
C56177 <C56177@test1.stargate-noe.enedis.fr>
2024-01-15 10:17:40 +0100

    Add an addpath function to help with PATH and LD_LIBRARY_PATH

--- bash/profile.sh
+++ bash/profile.sh
@@ -27,3 +27,57 @@ magenta='\[\e[0;35m\]'
 green='\[\e[0;32m\]'
 cyan='\[\e[0;36m\]'
 export PS1="${red}[\t] ${blue}\u${white}@${magenta}\h ${cyan}\$(parse_git_branch)${green}\w ${white}> "
+
+function addpath() {
+    args=()
+    variables=("PATH")
+    append=false
+    remove=false
+    while [[ "$#" -gt 0 ]]; do
+        case $1 in
+            -h|--help)
+                cat <<EOF
+Adds the input folders to the PATH or LD_LIBRARY_PATH env variables
+
+By default, the paths are added only to PATH
+
+Usage :
+      addpath [OPTIONS] FOLDERS_TO_ADD
+
+Options :
+        -h, --help   : Displays this help message
+        -a, --append : The paths are appended instead of prepended
+        -r, --remove : The paths are removed instead of added
+        --and-ld     : The paths are added both to PATH and LD_LIBRARY_PATH
+        --ld         : The paths are added only to LD_LIBRARY_PATH, not to PATH
+EOF
+                return 0
+                ;;
+            -a|--append) append=true;;
+            -r|--remove) remove=true;;
+            --and-ld) variables=("PATH" "LD_LIBRARY_PATH");;
+            --ld) variables=("LD_LIBRARY_PATH");;
+            *)
+                if [ -d "$1" ]; then
+                    args+=("$1");
+                else
+                    echo "WARNING : Ignoring folder $1 which does not exist"
+                fi ;;
+        esac
+        shift
+    done
+    for arg in "${args[@]}"; do
+        for var in "${variables[@]}"; do
+            if [[ $remove = true ]]; then
+                eval $var=$(echo ${!var} | sed "s|:*$arg:*|:|g" | sed 's/:$//g' | sed 's/^:*//g')
+            elif [[ ":${!var}:" != *":$arg:"* ]]; then
+                if [[ $append == true ]]; then
+                    eval $var="${!var:+${!var}:}$arg"
+                else
+                    eval $var="$arg${!var:+:${!var}}"
+                fi
+                export $var
+            fi
+        done
+    done
+}
