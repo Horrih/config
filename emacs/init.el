215d80614e5cd5fb28d2ec5544bc38075b9ac6cd
Charles Gonnaud <uyo@hotmail.fr>
2024-05-10 15:47:51 +0200

    Add corfu in the minibuffer

--- emacs/init.el
+++ emacs/init.el
@@ -784,9 +784,19 @@ This can be useful in conjunction to your project's variables defined in .dir-lo
   :custom
   (corfu-auto t)  ; Auto completion as you type (can be triggered manyally with completion-at-point)
   (corfu-cycle t) ; Cycle when at bottom of candidates
+  :bind (:map corfu-map ("M-c" . corfu-insert-separator))
   :init
   (require 'corfu-info)  ; M-g/M-h to see location/help for candidate
   (global-corfu-mode))   ; Enable corfu everywhere
+
+;;;;; corfu in the minibuffer
+(defun my/corfu-minibuffer ()
+  "Enable Corfu in the minibuffer."
+  (when (local-variable-p 'completion-at-point-functions)
+    (setq-local corfu-echo-delay nil ;; Disable automatic echo and popup
+                corfu-popupinfo-delay nil)
+    (corfu-mode 1)))
+(add-hook 'minibuffer-setup-hook #'my/corfu-minibuffer)
 
 ;;;; flymake : Syntax highlighting, used by eglot/lsp
 (use-package flymake
