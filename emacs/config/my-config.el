f1c283c9a203b0e9a5c1d58cd84e22a3f9ba92bc
Charles Gonnaud <uyo@hotmail.fr>
2024-09-14 21:33:01 +0200

    Use an customizable alist of enabled modules

--- emacs/config/my-config.el
+++ emacs/config/my-config.el
@@ -1,40 +1,20 @@
-(let ((config-dir (file-name-directory load-file-name))
-      (modules '(
-                 "bootstrap/my-bootstrap.el"
-                 "bootstrap/my-straight.el"
-                 ;; "bootstrap/my-esup.el"
-                 "editor/my-settings.el"
-                 "editor/my-plugins.el"
-                 "editor/my-commands.el"
-                 "editor/asmr.el"
-                 "looks/my-settings.el"
-                 "looks/my-theme.el"
-                 "looks/my-auto-margin.el"
-                 "looks/my-doom-modeline.el"
-                 "keybindings/my-base.el"
-                 "keybindings/my-ijkl.el"
-                 "keybindings/my-keybindings.el"
-                 "keybindings/my-transient.el"
-                 ;; "keybindings/my-tabs.el"
-                 "org/my-org-mode.el"
-                 "org/my-notes.el"
-                 "org/my-present.el"
-                 "prog/my-settings.el"
-                 "prog/my-compilation.el"
-                 "prog/my-commands.el"
-                 "prog/my-git.el"
-                 "prog/my-plugins.el"
-                 "prog/my-markdown.el"
-                 "prog/my-web.el"
-                 "prog/my-c++.el"
-                 "prog/my-python.el"
-                 "prog/my-yaml.el"
-                 "prog/my-docker.el"
-                 "prog/my-outline.el"
-                 "prog/my-corfu.el"
-                 "prog/my-linters.el"
-                 "prog/my-eglot.el"
-                 "prog/my-lsp-mode.el"
-                 )))
-  (mapcar (lambda(filename) (load (concat config-dir filename))) modules))
+(defcustom my/config-modules
+  '((bootstrap   (bootstrap straight))
+    (editor      (settings plugins commands asmr))
+    (looks       (settings theme auto-margin doom-modeline))
+    (keybindings (base ijkl keybindings transient))
+    (org         (org-mode notes present))
+    (prog        (settings compilation commands git plugins markdown
+                  web c++ python yaml docker outline corfu linters eglot lsp-mode)))
+  "Enabled init modules. Can be modified to
+- disable modules that are enabled by default here
+- enable additional modules that are disabled by default here"
+  :type '(alist :key-type symbol :value-type (repeat symbol)))
 
+(defvar my/config-dir (file-name-directory load-file-name)
+  "The directory containing the various configuration elisp files")
+
+(dolist (pair my/config-modules)
+  (let ((dir (car pair)))
+    (dolist (file (cadr pair))
+      (load (format "%s%s/my-%s.el" my/config-dir dir file)))))
