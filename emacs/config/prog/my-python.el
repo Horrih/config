9a95aaa329663636416031d48a5c79544a7b6ba8
Charles Gonnaud <uyo@hotmail.fr>
2024-12-09 16:30:11 +0100

    Add black formatting to python

--- emacs/config/prog/my-python.el
+++ emacs/config/prog/my-python.el
@@ -1,7 +1,7 @@
 ;;; legacy python mode
 (use-package python-mode
   :straight (:type built-in)
-  :hook (python-mode . (lambda() (setq-local fill-column 79))))
+  :hook (python-mode . (lambda() (setq-local fill-column 88))))
 
 ;;; code coverage
 (defun my/toggle-coverage-overlay()
@@ -17,3 +17,6 @@
   :config
   (require 'magit))  ; Uses magit's faces
 
+;;; Formatter black
+(use-package blacken
+  :hook (python-mode . (lambda() (blacken-mode))))
